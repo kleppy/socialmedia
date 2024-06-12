0d0b58aa3390b9bfdc7fc3b11ab8ae41cdf8d18c
J H <klep@Js-MacBook-Pro.attlocal.net>
2024-06-12 04:54:50 -0500

    added index.js to models to initialize all models

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// initialize video
+const Video = require('./Thought');
+// initialize Reaction
+const Reaction = require('./Reaction');
+// Initialize User
+const User = require('./User');
+// Export
+module.exports = { User, Video, Reaction };
